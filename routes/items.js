52ff51471844e6a60571e7873c501e5a4dc9c985
Gerard Hynes <gkhynes@gmail.com>
2021-12-16 17:16:09 +0000

    Add options, validation schemas and handlers

--- routes/items.js
+++ routes/items.js
@@ -1,15 +1,49 @@
 const items = require("../Items");
 
-function itemRoutes(fastify, options, done) {
-  fastify.get("/items", (req, reply) => {
-    reply.send(items);
-  });
+// Item schema
+const Item = {
+  type: "object",
+  properties: {
+    id: { type: "string" },
+    name: { type: "string" }
+  }
+};
 
-  fastify.get("/items/:id", (req, reply) => {
+// Options for getting all items
+const getItemsOpts = {
+  schema: {
+    response: {
+      200: {
+        type: "array",
+        items: Item
+      }
+    }
+  },
+  handler: function (req, reply) {
+    reply.send(items);
+  }
+};
+
+// Options for getting one item
+const getItemOpts = {
+  schema: {
+    response: {
+      200: Item
+    }
+  },
+  handler: function (req, reply) {
     const { id } = req.params;
     const item = items.find((item) => item.id == id);
     reply.send(item);
-  });
+  }
+};
+
+function itemRoutes(fastify, options, done) {
+  // Get all items
+  fastify.get("/items", getItemsOpts);
+
+  // Get single item
+  fastify.get("/items/:id", getItemOpts);
 
   done();
 }
